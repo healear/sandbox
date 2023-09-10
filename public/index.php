3877f50dd629731c00faa7b94c6a26a1b8e5e7cb
Maksim Sheshin <sheshin_ms@drom.ru>
2023-09-10 13:21:56 +1000

    Настроить обработку запросов через симфони

--- public/index.php
+++ public/index.php
@@ -1,9 +1,17 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+
+    // Обработка запроса через Symfony
+    $request = Request::createFromGlobals();
+    $response = $kernel->handle($request);
+    $response->send();
+
+    $kernel->terminate($request, $response);
 };
